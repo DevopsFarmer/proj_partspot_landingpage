f9c43ff5dec7bb1b237bc932562954674850fe6e
Navdeep <navdeepsinghbhati2562@gmail.com>
2025-07-30 20:02:34 +0530

    store file cred added

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,6 +24,10 @@ android {
 
     signingConfigs {
         create("release") {
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
         }
     }
 
